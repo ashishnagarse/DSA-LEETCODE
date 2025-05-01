82479178ff608651cf216bf30f126ea8eed439b0
ashish_nagarse <71747725+ashishnagarse@users.noreply.github.com>
2025-05-01 15:48:55 +0530

    Update 2071. Maximum Number of Tasks You Can Assign.cpp

--- Binary Search/2071. Maximum Number of Tasks You Can Assign.cpp
+++ Binary Search/2071. Maximum Number of Tasks You Can Assign.cpp
@@ -1,6 +1,4 @@
-/*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=7tGM7XA06MI
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/maximum-number-of-tasks-you-can-assign
 */
@@ -66,3 +64,61 @@ public:
 
     }
 };
+
+
+///////////////////////////////
+#include <vector>
+#include <algorithm>
+#include <deque>
+using namespace std;
+
+class Solution {
+public:
+    bool canAssign(int k, vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        deque<int> availableWorkers;
+        int m = workers.size();
+
+        // Take the strongest k workers
+        for (int i = m - k; i < m; ++i) {
+            availableWorkers.push_back(workers[i]);
+        }
+
+        int pillsUsed = 0;
+        for (int i = k - 1; i >= 0; --i) {
+            int taskStrength = tasks[i];
+            if (!availableWorkers.empty() && availableWorkers.back() >= taskStrength) {
+                // Assign the strongest worker without pill
+                availableWorkers.pop_back();
+            } else {
+                // Try to assign the weakest one with a pill
+                if (pillsUsed >= pills || availableWorkers.empty() || availableWorkers.front() + strength < taskStrength) {
+                    return false;
+                }
+                // Use pill on the weakest
+                availableWorkers.pop_front();
+                pillsUsed++;
+            }
+        }
+        return true;
+    }
+
+    int maxTasks(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        sort(tasks.begin(), tasks.end());
+        sort(workers.begin(), workers.end());
+
+        int left = 0, right = min((int)tasks.size(), (int)workers.size()), ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (canAssign(mid, tasks, workers, pills, strength)) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
+
